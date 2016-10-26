119ff079d78a60e228455c87bbb34f1d5a14ef41
mitchell mugele <mitchellmogz@hotmail.com>
2016-10-26 11:15:30 -0500

    First  Commit

---
+++ Season2/MrMeeseeks/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrMeeseeks
+{
+    class Box
+    {
+        static void Main(string[] args)
+        {
+
+            //Add Box Here
+
+
+        }
+    }
+}
