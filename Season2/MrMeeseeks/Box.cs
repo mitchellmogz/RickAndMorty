5c5450fb2fae62ceeab8c477cb352870f00091b1
mitchell mugele <mitchellmogz@hotmail.com>
2016-10-26 11:33:07 -0500

    Added input/output

--- Season2/MrMeeseeks/Box.cs
+++ Season2/MrMeeseeks/Box.cs
@@ -16,6 +16,36 @@ namespace MrMeeseeks
             string anyKey = Console.ReadLine();
 
             Console.WriteLine("I'm Mr. Meeseeks! Look at me!");
+            string input = Console.ReadLine();
+
+            if (input == "I want to be popular aat school" || input == "popular" || input == "popular at school")
+            {
+                Console.WriteLine("Ooh, okay!");
+            }
+
+            if (input == "I want to be a more complete woman" || input == "complete" || input == "complete woman")
+            {
+                Console.WriteLine("Oh, yeah! Yes, ma'am!");
+            }
+
+            if (input == "I would like to take two strokes off my golf game" || input == "two strokes" || input == "golf")
+            {
+
+                Console.WriteLine("Ooh yeah! Can do!");
+
+                Console.WriteLine("Press any key to swing.");
+                anyKey = Console.ReadLine();
+
+                Console.WriteLine("Remeber to square your shoulders, Jerry.");
+
+                Console.WriteLine("Press any key to swing.");
+                anyKey = Console.ReadLine();
+
+                Console.WriteLine("Try again and keep your head down.");
+
+            }
+
+
 
         }
     }
